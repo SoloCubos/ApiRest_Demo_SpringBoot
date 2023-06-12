9b3c4bad3132a586a64820fd1b21050f5ee4d1b1
SoloCubos <solanofernando35@gmail.com>
2023-06-11 23:19:36 -0500

    Metodo Delete y Update Creados

--- src/main/java/com/example/apirest/demoapirest/controllers/PersonaController.java
+++ src/main/java/com/example/apirest/demoapirest/controllers/PersonaController.java
@@ -5,10 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import com.example.apirest.demoapirest.entities.Persona;
 import com.example.apirest.demoapirest.services.PersonaService;
@@ -39,11 +44,30 @@ public class PersonaController {
         }
     }
 
-    public ResponseEntity<?> save(Persona persona){
+    @PostMapping("")
+    public ResponseEntity<?> save(@RequestBody Persona persona){
         try {
             return ResponseEntity.status(HttpStatus.OK).body(personaService.save(persona));
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error 69, Por favor intente mas tarde.\"}");
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody Persona persona){
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(personaService.update(id, persona));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error 69, Por favor intente mas tarde.\"}");
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id){
+        try {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(personaService.delete(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error 69, Por favor intente mas tarde.\"}");
         }
     }
 }
